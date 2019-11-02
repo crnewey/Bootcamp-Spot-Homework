c46bb93465cdf2984d69722d0a26652976981b4c
captain america <52713662+crnewey@users.noreply.github.com>
2019-11-02 17:05:11 -0400

    ugh2

--- HW 12 web-scraping-challenge/app.py
+++ HW 12 web-scraping-challenge/app.py
@@ -19,62 +19,6 @@ def home():
     print(Mars_data)
     return render_template("index.html", Mars_Info=Mars_data)
 
-# # def render_index():
-#     # destination_data = mongo.db.collection.find_one()
-#     # Error handler for missing collection
-#     mars_find =  client.db.collection.find_one()
-#     return render_template("index.html.", mars=mars_find)
-
-#     # try:
-#     #     mars_find =  mars_collection.find_one()
-#     #     # Distributes data from collection
-#     #     news_title = mars_find['news_data']['news_title']
-#     #     paragraph_text_1 = mars_find['news_data']['paragraph_text_1']
-#     #     paragraph_text_2 = mars_find['news_data']['paragraph_text_2']
-#     #     featured_image_url = mars_find['featured_image_url']
-#     #     mars_weather_tweet = mars_find['mars_weather']
-    #     mars_facts_table = mars_find['mars_facts']
-    #     hemisphere_title_1 = mars_find['mars_hemispheres'][0]['title']
-    #     hemisphere_img_1 = mars_find['mars_hemispheres'][0]['img_url']
-    #     hemisphere_title_2 = mars_find['mars_hemispheres'][1]['title']
-    #     hemisphere_img_2 = mars_find['mars_hemispheres'][1]['img_url']
-    #     hemisphere_title_3 = mars_find['mars_hemispheres'][2]['title']
-    #     hemisphere_img_3 = mars_find['mars_hemispheres'][2]['img_url']
-    #     hemisphere_title_4 = mars_find['mars_hemispheres'][3]['title']
-    #     hemisphere_img_4 = mars_find['mars_hemispheres'][3]['img_url']
-    # except (IndexError, TypeError) as error_handler:
-    #     # Missing collection; clears fields
-    #     news_title = ""
-    #     paragraph_text_1 =""
-    #     paragraph_text_2 = ""
-    #     featured_image_url = ""
-    #     mars_weather_tweet = ""
-    #     mars_facts_table = ""
-    #     hemisphere_title_1 = ""
-    #     hemisphere_img_1 = ""
-    #     hemisphere_title_2 = ""
-    #     hemisphere_img_2 = ""
-    #     hemisphere_title_3 = ""
-    #     hemisphere_img_3 = ""
-    #     hemisphere_title_4 = ""
-    #     hemisphere_img_4 = ""
-
-    # # Renders template to index.html
-    # return render_template("index.html", news_title=news_title,\
-    #                                      paragraph_text_1=paragraph_text_1,\
-    #                                      paragraph_text_2=paragraph_text_2,\
-    #                                      featured_image_url=featured_image_url,\
-    #                                      mars_weather_tweet=mars_weather_tweet,\
-    #                                      mars_facts_table=mars_facts_table,\
-    #                                      hemisphere_title_1=hemisphere_title_1,\
-    #                                      hemisphere_img_1=hemisphere_img_1,\
-    #                                      hemisphere_title_2=hemisphere_title_2,\
-    #                                      hemisphere_img_2=hemisphere_img_2,\
-    #                                      hemisphere_title_3=hemisphere_title_3,\
-    #                                      hemisphere_img_3=hemisphere_img_3,\
-    #                                      hemisphere_title_4=hemisphere_title_4,\
-    #                                      hemisphere_img_4=hemisphere_img_4)
-
 # Initializes scrape route; inserts results into  mars_data_DB in MongoDB
 @app.route('/scrape')
 def scrape():
